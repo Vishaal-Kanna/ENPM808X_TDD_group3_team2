591e5231c6bf4f61a5dece8cdcb0e36470395820
Sanchit Kedia <sanchit@terpmail.umd.edu>
2022-10-04 19:10:34 -0400

    Initial Implementation of computVelocity function

--- app/pid.cpp
+++ app/pid.cpp
@@ -9,50 +9,59 @@
  * 
  */
 
- #include<iostream>
- #include "../include/pid.hpp"
+#include <iostream>
+#include <cmath>
+#include "../include/pid.hpp"
 
- void pid::PID::setKp(double val)
- {
+void pid::PID::setKp(double val) {
     _Kp = val;
- }
+}
 
- double pid::PID::getKp()
- {
-    return -1;
- }
+double pid::PID::getKp() {
+    return _Kp;
+}
 
- void pid::PID::setKd(double val)
- {
+void pid::PID::setKd(double val) {
     _Kd = val;
- }
+}
 
- double pid::PID::getKd()
- {
-    return -1;
- }
+double pid::PID::getKd() {
+    return _Kd;
+}
 
- void pid::PID::setKi(double val)
- {
+void pid::PID::setKi(double val) {
     _Ki = val;
- }
+}
 
- double pid::PID::getKi()
- {
-    return -1;
- }
+double pid::PID::getKi() {
+    return _Ki;
+}
 
- void pid::PID::setTstep(float val)
- {
+void pid::PID::setTstep(float val) {
     _tstep = val;
- }
+}
 
-float pid::PID::getTstep()
- {
-    return -1;
- }
+float pid::PID::getTstep() {
+    return _tstep;
+}
 
- double pid::PID::computeVelocity(double V_target, double V_current)
- {
-    return 3.5;
- }
+double pid::PID::computeVelocity(double V_target, double V_current) {
+  double error = V_target - V_current;
+  double previous_error = 0.0;
+  double sum_error = 0.0;
+  double feedback = 0.0;
+
+  while (std::abs(error) > 0.1) {
+    error = V_target - V_current;
+
+    sum_error += error;
+
+    double p = _Kp * error;
+    double i = _Ki * sum_error * _tstep;
+    double d = _Kd * (error - previous_error) / _tstep;
+    feedback = p + i + d;
+    V_current = feedback * _tstep;
+    previous_error = error;
+  }
+  return feedback;
+}
